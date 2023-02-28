11c315f3b2abad28694b742b633af3b075db01a5
josemartinmolina <jose.molina@tec.mx>
2023-02-28 16:02:01 -0600

    Cambios en clase martes

--- calculadora/views.py
+++ calculadora/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def nueva():
     return 0
+def index(request):
+    #return HttpResponse("Bienvenido")
+    return render(request,'index.html')
+
+def procesamiento(request):
+    nombre = request.POST['nombre']
+    nombre = nombre.title()
+    return render(request, 'proceso.html', {'name':nombre})
